0cf44e896b2f7954eb93b4feae02e4826c883fc1
Konstantine <Konstaphy@gmail.com>
2022-06-13 15:11:15 +0500

    готово

--- main.py
+++ main.py
@@ -1,60 +1,59 @@
 from tkinter import *
-from PIL import ImageTk, Image
-from tkinter import filedialog
+from image import ImagePresentation
+
+filters = ["median", "middle"]
+salts = ["s&p", "bgsh"]
 
 
-def openfilename():
-    # open file dialog box to select image
-    # The dialogue box has a title "Open"
-    filename = filedialog.askopenfilename(title='"pen')
-    return filename
+class App:
+    def __init__(self):
+        self.list_filters = None
+        self.list = None
+        self.frame = None
+        self.root = Tk()
+        self.root_setting()
+        self.image_presentation = ImagePresentation(self.root)
+        self.load_buttons()
+        self.load_salt_select()
+        self.load_filter_select()
+        self.root.mainloop()
+
+    def root_setting(self):
+        self.root['bg'] = '#fafafa'
+        self.root.geometry("800x700")
+        self.root.resizable(width=False, height=False)
+        self.frame = Frame(self.root)
+        self.frame.place(relwidth=1, relheight=1)
+
+    def load_buttons(self):
+        btn_load = Button(self.frame, text="load image", command=self.image_presentation.open)
+        btn_load.pack()
+
+        btn_salt = Button(self.frame,
+                          text="make it salty",
+                          command=lambda: self.image_presentation.salty(0.01, self.list.curselection()))
+        btn_salt.pack()
+
+        btn_filter = Button(self.frame, text="filter da salt",
+                            command=self.image_presentation.filter)
+        btn_filter.pack()
+
+        btn_save = Button(self.frame, text="save image", command=self.image_presentation.save_image)
+        btn_save.pack()
+
+    def load_salt_select(self):
+        self.list = Listbox(self.frame)
+        for salt in salts:
+            self.list.insert(END, salt)
+
+        self.list.pack()
+
+    def load_filter_select(self):
+        self.list_filters = Listbox(self.frame)
+        for filter_section in filters:
+            self.list_filters.insert(END, filter_section)
+
+        self.list_filters.pack()
 
 
-
-def open_img():
-    # Select the Imagename  from a folder
-    x = openfilename()
-
-    # opens the image
-    img = Image.open(x)
-
-    # resize the image and apply a high-quality down sampling filter
-    img = img.resize((128, 128), Image.ANTIALIAS)
-
-    # PhotoImage class is used to add image to widgets, icons etc
-    img = ImageTk.PhotoImage(img)
-
-    # create a label
-    panel = Label(root, image=img)
-
-    # set the image as img
-    panel.image = img
-    panel.grid(row=2)
-
-
-def make_salty():
-    print("huy")
-
-
-root = Tk()
-
-root['bg'] = '#fafafa'
-root.geometry("500x400")
-root.resizable(width=False, height=False)
-
-frame = Frame(root)
-frame.place(relwidth=1, relheight=1)
-
-btn = Button(frame, text="load image", command=open_img)
-btn.pack()
-
-btn = Button(frame, text="make it salty", command=make_salty)
-btn.pack()
-
-btn = Button(frame, text="filter da salt", command=make_salty)
-btn.pack()
-
-btn = Button(frame, text="save image", command=make_salty)
-btn.pack()
-
-root.mainloop()
+app = App()
